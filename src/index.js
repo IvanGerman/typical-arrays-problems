5432fc6c674d5a1fefd91cfea8569049559eb442
IvanGerman <ivgvi@mail.ru>
2021-08-20 22:07:49 +0700

    feat: solved task 'typical array problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array === undefined || array.length === 0 ) {
+    return 0;
+  };
+
+  let minNumber = array[0];
+  for ( let i = 0; i < array.length - 1; i++ ) {
+      if ( minNumber < array[i+1] ) {
+          minNumber = minNumber;
+      } else {
+          minNumber = array[i+1];
+      }
+  }
+
+    return minNumber ;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length === 0 ) {
+    return 0;
+  };
+
+  let maxNumber = array[0];
+  for ( let i = 0; i < array.length - 1; i++ ) {
+      if ( maxNumber > array[i+1] ) {
+          maxNumber = maxNumber;
+      } else {
+          maxNumber = array[i+1];
+      }
+  }
+
+    return maxNumber ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length === 0 ) {
+    return 0;
+  };
+
+  let avgNumber = 0;
+  let sum = 0;
+  for ( let i = 0; i < array.length; i++ ) {
+      sum += array[i];
+  }
+  avgNumber = sum / array.length;
+  return avgNumber ;
 }
